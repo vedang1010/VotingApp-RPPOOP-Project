246d25af46a6a1c35f7163f5b04ff74d4fefab41
Vedang <vedangkhedekar07@gmail.com>
2023-05-23 17:40:37 +0530

    SignUp and Login Done

--- app/src/main/java/com/example/myapplication/CreatePollActivity.java
+++ app/src/main/java/com/example/myapplication/CreatePollActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,7 +27,7 @@ public class CreatePollActivity extends AppCompatActivity {
         addOptionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addOptionEditText();
+                addOption();
             }
         });
 
@@ -38,27 +39,46 @@ public class CreatePollActivity extends AppCompatActivity {
         });
     }
 
-    private void addOptionEditText() {
+    private void addOption() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        LinearLayout optionLayout = new LinearLayout(this);
+        optionLayout.setLayoutParams(layoutParams);
+        optionLayout.setOrientation(LinearLayout.HORIZONTAL);
+
         EditText optionEditText = new EditText(this);
         optionEditText.setLayoutParams(new LinearLayout.LayoutParams(
-//                LinearLayout.LayoutPara
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        optionEditText.setHint("enter candidate name");
-        optionsLayout.addView(optionEditText);
+                0, LinearLayout.LayoutParams.WRAP_CONTENT, 1));
+        optionEditText.setHint("Enter candidate name");
+
+        Button deleteButton = new Button(this);
+        deleteButton.setText("Delete");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                optionsLayout.removeView(optionLayout);
+            }
+        });
+
+        optionLayout.addView(optionEditText);
+        optionLayout.addView(deleteButton);
+
+        optionsLayout.addView(optionLayout);
     }
 
     private void createPoll() {
         String question = ((EditText) findViewById(R.id.questionEditText)).getText().toString();
         int optionCount = optionsLayout.getChildCount();
 
-        // Store
         // Store the question and options in your database or perform any desired action
         // For simplicity, let's just print the question and options to the console
         System.out.println("Question: " + question);
         System.out.println("Options:");
         for (int i = 0; i < optionCount; i++) {
-            EditText optionEditText = (EditText) optionsLayout.getChildAt(i);
+            LinearLayout optionLayout = (LinearLayout) optionsLayout.getChildAt(i);
+            EditText optionEditText = (EditText) optionLayout.getChildAt(0);
             String option = optionEditText.getText().toString();
             System.out.println("- " + option);
         }
--- app/src/main/java/com/example/myapplication/logInActivity.java
+++ app/src/main/java/com/example/myapplication/logInActivity.java
@@ -58,7 +58,7 @@ public class logInActivity extends AppCompatActivity {
 
                         @Override
                         public void onCancelled(@NonNull DatabaseError error) {
-
+                            Toast.makeText(logInActivity.this, "Database error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
